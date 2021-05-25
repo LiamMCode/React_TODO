1bb8dace21960d2cde8766c287f45f4877317137
Liam <81374620+LiamMCode@users.noreply.github.com>
2021-05-25 16:29:49 +0100

    Made a big redesign on Todo App
    the app now renders and shows all elements of DOM on screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>Todo App</h1>
+      <form>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>Hide Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Remove All</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Clear Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">
+        Tasks
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-0" type="checkbox" defaultChecked={false} />
+            <label className="todo-label" htmlFor="todo-0">
+            Implement the addTodo method
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Implement the addTodo method</span>
+            </button>
+          </div>
+        </li>
+
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-1" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-1">
+            Implement the removeTodo method
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Implement the removeTodo method</span>
+            </button>
+          </div>
+        </li>
+
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-2" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-2">
+            Implement the clearCompletedTodos method
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Implement the clearCompletedTodos method</span>
+            </button>
+          </div>
+        </li>
+
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-3" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-3">
+            Implement the removeAllTodos method
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Implement the removeAllTodos method</span>
+            </button>
+          </div>
+        </li>
+
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-4" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-4">
+            Implement the showHideCompletedTodos method
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Implement the showHideCompletedTodos method</span>
+            </button>
+          </div>
+        </li>
+
+        <li className="todo stack-small">
+          <div className="c-cb">
+            <input id="todo-5" type="checkbox" />
+            <label className="todo-label" htmlFor="todo-5">
+            Implement the toggleTodoCompleteStatus method
+            </label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete <span className="visually-hidden">Implement the toggleTodoCompleteStatus method</span>
+            </button>
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,70 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AddTodoForm from './AddTodoForm';
-import TodoLayout from './todoLayout';
-import {shuffle, sample} from 'underscore';
-import { BrowserRouter, Route, withRouter} from 'react-router-dom';
-
-const todos = [
-  {
-    name: 'Implement the addTodo method',
-  }, 
-  {
-    name: 'Implement the removeTodo method',
-  }, 
-  {
-    name: 'Implement the clearCompletedTodos method',
-  },
-  {
-    name: 'Implement the removeAllTodos method', 
-  }, 
-  {
-    name: 'Implement the showHideCompletedTodos method', 
-  }, 
-  {
-    name: 'Implement the toggleTodoCompleteStatus method',
-  }
-]
-
-const TodoWrapper = withRouter(({ history }) => 
-  <AddTodoForm onAddTodo={(todos) => {
-    todos.push(todos);
-    history.push('/');
-  }} />
-);
-
-let state = resetState();
-
-function resetState() {
-  return {
-    todoList: getTodoList(todos)
-  };
-}
-
-function getTodoList(todos) {
-  const allTodos = todos.reduce(function (p,c,i) {
-    return p.concat(c.name);
-  }, []);
-
-  return {
-    name: allTodos
-  }
-}
-
-function App() {
-  return <TodoLayout {...state} 
-    onContinue={() => {
-      state = resetState();
-      render();
-  }} />;
-}
+import App from './App'
+import { BrowserRouter, Route} from 'react-router-dom';
 
 function render() {
   ReactDOM.render(<BrowserRouter>
     <React.Fragment>
       <Route exact path='/' component={App} /> 
-      <Route path='/add' component={TodoWrapper} />
     </React.Fragment>
     </BrowserRouter>, document.getElementById('root')
   );
